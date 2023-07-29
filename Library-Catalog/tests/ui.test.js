294f5537e46ed4edb22b5009a97f8dfe3363fb5c
Laz <lazgerasimov@gmail.com>
2023-07-29 20:58:56 +0100

    add My Books visibility after login test

--- Library-Catalog/tests/ui.test.js
+++ Library-Catalog/tests/ui.test.js
@@ -46,5 +46,17 @@ test('Verify "All Books" link is visible after login', async ({ page }) => {
     expect(isAllBooksLinkVisible).toBe(true);
 });
 
+test('Verify "My Books" link is visible after login', async ({ page }) => {
+
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    const myBooksLink = await page.$('a[href="/profile"]');
+    const isMyBooksLinkVisible = await myBooksLink.isVisible();
+
+    expect(isMyBooksLinkVisible).toBe(true);
+});
 
 
